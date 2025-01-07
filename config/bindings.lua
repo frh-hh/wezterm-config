af5bc3d293a4b898b5299b3fcd0bf2581f63a7e1
frh-hh <2478804720@qq.com>
2025-01-07 09:45:41 +0800

    refactor: 修改粘贴键位

--- config/bindings.lua
+++ config/bindings.lua
@@ -55,7 +55,7 @@ local keys = {
 
    -- copy/paste --
    { key = 'c',          mods = 'CTRL|SHIFT',  action = act.CopyTo('Clipboard') },
-   { key = 'v',          mods = 'CTRL|SHIFT',  action = act.PasteFrom('Clipboard') },
+   { key = 'v',          mods = 'CTRL',        action = act.PasteFrom('Clipboard') },
 
    -- tabs --
    -- tabs: spawn+close
